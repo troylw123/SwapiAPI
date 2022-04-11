486ea625c0eebfab8a32780af72744ae24ddfe3b
Troy Weaver <troylw@gmail.com>
2022-04-11 15:12:29 -0400

    Updated People View

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SwapiMVC.Models;
 
 namespace SwapiMVC.Controllers
 {
@@ -19,9 +21,13 @@ namespace SwapiMVC.Controllers
             _httpClient = httpClientFactory.CreateClient("swapi");
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string page)
         {
-            return View();
+            string route = $"people?page={page ?? "1"}";
+            HttpResponseMessage response = await _httpClient.GetAsync(route);
+            var responseString = await response.Content.ReadAsStringAsync();
+            var people = JsonSerializer.Deserialize<ResultsViewModel<PeopleViewModel>>(responseString);
+            return View(people);
         }
     }
 }
